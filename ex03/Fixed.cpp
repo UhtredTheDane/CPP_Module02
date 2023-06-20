82db3383c56049b404225f085f53bfffddb90448
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2023-06-21 01:58:37 +0200

    base ex03

---
+++ ex03/Fixed.cpp
@@ -0,0 +1,166 @@
+#include "Fixed.hpp"
+
+int const Fixed::bits_nb = 8;
+
+Fixed::Fixed(void)
+{
+	value = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(int const int_value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	value = int_value << bits_nb;
+}
+
+Fixed::Fixed(float const float_value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	value = roundf(float_value * (1 << bits_nb));
+}
+
+Fixed::Fixed(Fixed const& toCopy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = toCopy;
+}
+
+Fixed& Fixed::operator=(Fixed const& toAffect)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &toAffect)
+		value = toAffect.value;
+	return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (value);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	value = raw;
+}
+
+int	Fixed::toInt(void) const
+{
+	return (value >> bits_nb);
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)value / (1 << bits_nb));
+}
+
+std::ostream &operator<<(std::ostream &flux, Fixed const& fixed_number)
+{
+	flux << fixed_number.toFloat();
+	return (flux);
+}
+
+bool	operator>(Fixed const& f1, Fixed const& f2)
+{
+	return (f1.getRawBits() > f2.getRawBits());
+}
+
+bool	operator<(Fixed const& f1, Fixed const& f2)
+{	
+	return (f1.getRawBits() < f2.getRawBits());
+}
+
+bool	operator>=(Fixed const& f1, Fixed const& f2)
+{	
+	return (f1.getRawBits() >= f2.getRawBits());
+}
+
+bool	operator<=(Fixed const& f1, Fixed const& f2)
+{	
+	return (f1.getRawBits() <= f2.getRawBits());
+}
+
+bool	operator==(Fixed const& f1, Fixed const& f2)
+{	
+	return (f1.getRawBits() == f2.getRawBits());
+}
+
+bool	operator!=(Fixed const& f1, Fixed const& f2)
+{	
+	return (!operator==(f1, f2));
+}
+
+Fixed	operator+(Fixed const& f1, Fixed const& f2)
+{	
+	return (f1 + f2);
+}
+
+Fixed	operator-(Fixed const& f1, Fixed const& f2)
+{	
+	return (f1 - f2);
+}
+
+Fixed&	Fixed::operator*=(const Fixed &fixed_number)
+{
+	this->value *= fixed_number.value >> bits_nb;
+	return (*this);
+}
+
+Fixed	operator*(Fixed const& f1, Fixed const& f2)
+{
+	Fixed	res(f1);
+
+	res *= f2;
+	return (res);
+}
+
+Fixed&	Fixed::operator/=(const Fixed &fixed_number)
+{
+	float	temp;
+
+	temp = this->value / fixed_number.value;
+	this->value = roundf(temp * (1 << bits_nb));
+	return (*this);
+}
+
+Fixed	operator/(Fixed const& f1, Fixed const& f2)
+{
+	Fixed	res(f1);
+
+	res /= f2;
+	return (res);
+}
+
+Fixed&	Fixed::operator++()
+{
+	this->value += 1; 
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed	temp(*this);
+
+	++*this;
+	return (temp);
+}
+
+Fixed&	Fixed::operator--()
+{
+	this->value -= (1 << bits_nb);
+	return (*this);
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed	temp(*this);
+
+	--*this;
+	return (temp);
+}
---
+++ ex03/Fixed.hpp
@@ -0,0 +1,46 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed(void);
+		Fixed(int const int_value);
+		Fixed(const float float_value);
+		Fixed(Fixed const& toCopy);
+		Fixed& operator=(Fixed const& toAffect); 
+		~Fixed(void);
+		int	getRawBits(void) const;
+		void	setRawBits(int const raw);
+		int	toInt(void) const;
+		float	toFloat(void) const;
+		Fixed&	operator*=(const Fixed &fixed_number);
+		Fixed&	operator/=(const Fixed &fixed_number);
+		Fixed&	operator++();
+		Fixed	operator++(int);
+		Fixed&	operator--();
+		Fixed	operator--(int);
+	private:
+		int	value;
+		static int const bits_nb;
+
+};
+
+std::ostream &operator<<(std::ostream &flux, Fixed const& fixed_number);
+bool	operator>(Fixed const& f1, Fixed const& f2);
+bool	operator<(Fixed const& f1, Fixed const& f2);
+bool	operator>=(Fixed const& f1, Fixed const& f2);
+bool	operator<=(Fixed const& f1, Fixed const& f2);
+bool	operator==(Fixed const& f1, Fixed const& f2);
+bool	operator!=(Fixed const& f1, Fixed const& f2);
+Fixed	operator+(Fixed const& f1, Fixed const& f2);
+Fixed	operator-(Fixed const& f1, Fixed const& f2);
+Fixed	operator*(Fixed const& f1, Fixed const& f2);
+Fixed	operator/(Fixed const& f1, Fixed const& f2);
+
+
+
+#endif
